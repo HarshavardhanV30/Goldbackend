d123f8784400ad54ae0e2bc78222e47ec34118f8
HarshavardhanV <168812589+HarshavardhanV30@users.noreply.github.com>
2026-09-23 22:57:26 +0530

    Implement logging for database connection status
    Add database connection logging to db.js

--- Nodejsbackend/db.js
+++ Nodejsbackend/db.js
@@ -1,8 +1,18 @@
 const { Pool } = require("pg");
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false,
   },
 });
+
+pool.query("SELECT NOW()", (err, result) => {
+  if (err) {
+    console.error("❌ DATABASE CONNECTION FAILED:", err.message);
+  } else {
+    console.log("✅ DATABASE CONNECTED:", result.rows[0]);
+  }
+});
+
 module.exports = pool;
